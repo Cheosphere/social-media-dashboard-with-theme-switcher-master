fbbfc259555a5cd055bddd78d4d36e6d9b8f0c13
Eliseo Avila <ebae82@hotmail.com>
2022-11-20 23:12:42 -0300

    Cargando proyecto en repositorio remoto

---
+++ README.md
@@ -0,0 +1,77 @@
+# Tablero de redes sociales con selector de temas
+
+El desafío es desarrollar este tablero y lograr que se parezca lo más posible al diseño.
+
+- HTML
+- CSS
+- JS Vanilla (with animejs library)
+
+![desktop-preview](https://user-images.githubusercontent.com/112582420/202934467-baa6a1bf-a6d8-4b9b-a476-fe4e32689471.jpg)
+
+# Guía de Estilo
+
+## Diseño
+
+Los diseños se crearon con los siguientes anchos:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colores
+### Primarios
+
+- Lime Green: hsl(163, 72%, 41%)
+- Bright Red: hsl(356, 69%, 56%)
+- Facebook: hsl(208, 92%, 53%)
+- Twitter: hsl(203, 89%, 53%)
+- Instagram: linear gradient hsl(37, 97%, 70%) to hsl(329, 70%, 58%)
+
+#### Tema Oscuro
+
+- Toggle: linear gradient hsl(210, 78%, 56%) to hsl(146, 68%, 55%)
+
+#### Tema Claro
+
+- Toggle: hsl(230, 22%, 74%)
+
+### Neutrales
+
+#### Tema Oscuro
+
+- Very Dark Blue (BG): hsl(230, 17%, 14%)
+- Very Dark Blue (Top BG Pattern): hsl(232, 19%, 15%)
+- Dark Desaturated Blue (Card BG): hsl(228, 28%, 20%)
+- Desaturated Blue (Text): hsl(228, 34%, 66%)
+- White (Text): hsl(0, 0%, 100%)
+
+#### Tema Claro
+
+- White (BG): hsl(0, 0%, 100%)
+- Very Pale Blue (Top BG Pattern): hsl(225, 100%, 98%)
+- Light Grayish Blue (Card BG): hsl(227, 47%, 96%)
+- Dark Grayish Blue (Text): hsl(228, 12%, 44%)
+- Very Dark Blue (Text): hsl(230, 17%, 14%)
+
+## Tipografía
+
+### Cuerpo
+
+- Font size : 14px
+
+### Fuentes
+
+- Family: [Inter](https://fonts.google.com/specimen/Inter)
+- Weights: 400, 700
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,62 @@
+const body = document.getElementById('body')
+const title = document.getElementById('title')
+const bottomTitle = document.getElementById('bottom_title')
+const headerBorder = document.getElementById('header_border')
+const toggleSwitch = document.getElementById('switch')
+const header = document.getElementById('header')
+const cards = document.querySelectorAll('.card')
+const bottomCards = document.querySelectorAll('.bottom_card')
+const followersNumber = document.querySelectorAll('.card_followers_number')
+const footerValue = document.querySelectorAll('.bottom_card_footer_value')
+
+
+toggleSwitch.addEventListener('click', () => {
+    body.classList.toggle('dark_body')
+    header.classList.toggle('dark_header')
+    title.classList.toggle('dark_title')
+    bottomTitle.classList.toggle('dark_bottom_title')
+    headerBorder.classList.toggle('dark_border')
+
+    cards.forEach(card => {
+        card.classList.toggle('dark_card')
+    })
+
+    bottomCards.forEach(card => {
+        card.classList.toggle('dark_card')
+    })
+
+    followersNumber.forEach(number => {
+        number.classList.toggle('dark_title')
+    })
+
+    footerValue.forEach(value => {
+        value.classList.toggle('dark_title')
+    })
+})
+
+anime({ targets: '.card1', innerHTML: [0, 1987], easing: 'linear', round: 1 });
+anime({ targets: '.card2', innerHTML: [0, 1044], easing: 'linear', round: 1 });
+anime({ targets: '.card3', innerHTML: [0, '11k'], easing: 'linear', round: 1 });
+anime({ targets: '.card4', innerHTML: [0, 8239], easing: 'linear', round: 1 });
+anime({ targets: '.b_card1', innerHTML: [0, 87], easing: 'linear', round: 1 });
+anime({ targets: '.b_card2', innerHTML: [0, 52], easing: 'linear', round: 1 });
+anime({ targets: '.b_card3', innerHTML: [0, 5462], easing: 'linear', round: 1 });
+anime({ targets: '.b_card4', innerHTML: [0, '52k'], easing: 'linear', round: 1 });
+anime({ targets: '.b_card5', innerHTML: [0, 117], easing: 'linear', round: 1 });
+anime({ targets: '.b_card6', innerHTML: [0, 507], easing: 'linear', round: 1 });
+anime({ targets: '.b_card7', innerHTML: [0, 107], easing: 'linear', round: 1 });
+anime({ targets: '.b_card8', innerHTML: [0, 1407], easing: 'linear', round: 1 });
+
+anime({
+    targets: '.card_footer, .bottom_card_footer_percent',
+    opacity: [0, 1],
+    translateY: [25, 0],
+    duration: 2000
+});
+
+anime({
+    targets: '.bottom_card_header, .card_logo',
+    opacity: [0, 1],
+    translateY: [-25, 0],
+    duration: 2000
+});
